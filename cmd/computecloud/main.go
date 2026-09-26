f953152d6bc4c3482c0d01f44617a8dbe17da930
tommyxie2026-tech <tommyxie2026@gmail.com>
2026-09-26 16:57:16 +0900

    release: target v0.3.1

--- cmd/computecloud/main.go
+++ cmd/computecloud/main.go
@@ -25,7 +25,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var version = "0.3.0"
+var version = "0.3.1"
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
